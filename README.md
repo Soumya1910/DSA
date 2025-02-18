ecbb6356bac841d435300da5417d11a2c3307b4b
Soumya Mukherjee <Soumya.Mukherjee@tesco.com>
2025-02-18 23:41:21 +0530

    NextLargest element from right

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # DSA
 This is a repository for Program and analysis on Data Structure and Algorithm
+
+# STACK
+
+## How to Identify?
+In Brute-force method, if second loop variable(j) is directly dependent on the first loop variable(i).
+
+## Problem Statements
+1. Nearest Greater/ Next Largest element to Left 
+2. Nearest Greater/ Next Largest element to Right [NextLargestElementToRight](.
+   /src/main/java/com/dsa/stack/NextLargestElementToRight.java)
+3. Nearest Smaller Element to Left
+4. Nearest Smaller Element to Right
+5. Stock Span Problem
+6. Maximum Area of Histogram
+7. Max area of Rectangle in Binary Matrix
+8. Rain Water Trapping
---
+++ src/main/java/com/dsa/stack/NextLargestElementToRight.java
@@ -0,0 +1,52 @@
+package com.dsa.stack;
+
+import java.util.*;
+
+/*
+* Logic: 1. We should traverse the array from right to left.
+*        2. If stack is empty then return -1
+*        3. If current array element is greater than the top of the stack element then pop the element till the
+* element from stack is greater than the array element OR stack is empty. Insert the array element into the stack.
+*        4. If current array element is lesser than the top of the stack element then return the top of the stack
+* element and insert the element into stack.
+* */
+
+public class NextLargestElementToRight {
+    public static void main(String[] args) {
+        List<Integer> elements = Arrays.asList(1, 3, 0, 0, 1, 2, 4); // Output: [3, 4, 1, 1, 2, 4, -1]
+        // List<Integer> elements = Arrays.asList(1, 3, 2, 4); // [3, 4, 4, -1]
+        List<Integer> output = findNextGreaterElement(elements);
+        Collections.reverse(output);
+        System.out.println(output);
+    }
+
+    private static List<Integer> findNextGreaterElement(List<Integer> element) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = element.size() - 1; i >= 0; i--) {
+            int e = element.get(i);
+            //System.out.println("Element is: " + e + "\tList: " + list);
+            //displayStack(stack);
+            if (stack.empty()) {
+                list.add(-1);
+                stack.push(e);
+            } else if (e >= stack.peek()) {
+                while (e >= stack.peek() && !stack.empty()) {
+                    stack.pop();
+                }
+                list.add(stack.pop());
+                stack.push(e);
+            } else {
+                list.add(stack.peek());
+                stack.push(e);
+            }
+        }
+        return list;
+    }
+
+    private static void displayStack(Stack<Integer> stack) {
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            System.out.println(stack.get(i));
+        }
+    }
+}
